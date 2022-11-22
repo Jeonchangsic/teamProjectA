f308088a1133fdef85ceb1913494a54b30d8ddc6
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2022-11-22 16:25:29 +0900

    tw

---
+++ teamProjectA/src/main/java/team/projectA/vo/QnaVO.java
@@ -0,0 +1,69 @@
+package team.projectA.vo;
+
+public class QnaVO extends UserVO{
+
+	private int QnA_idx;
+	private int uidx;
+	private String usertype;
+	private String qna_Qcontent;
+	private String qna_Qtitle;
+	private String qna_Acheck;
+	private String qna_Acontent;
+	private String qna_Qdate;
+	private String qna_Adate;
+	public int getQnA_idx() {
+		return QnA_idx;
+	}
+	public void setQnA_idx(int qnA_idx) {
+		QnA_idx = qnA_idx;
+	}
+	public int getUidx() {
+		return uidx;
+	}
+	public void setUidx(int uidx) {
+		this.uidx = uidx;
+	}
+	public String getUsertype() {
+		return usertype;
+	}
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+	public String getQna_Qcontent() {
+		return qna_Qcontent;
+	}
+	public void setQna_Qcontent(String qna_Qcontent) {
+		this.qna_Qcontent = qna_Qcontent;
+	}
+	public String getQna_Qtitle() {
+		return qna_Qtitle;
+	}
+	public void setQna_Qtitle(String qna_Qtitle) {
+		this.qna_Qtitle = qna_Qtitle;
+	}
+	public String getQna_Acheck() {
+		return qna_Acheck;
+	}
+	public void setQna_Acheck(String qna_Acheck) {
+		this.qna_Acheck = qna_Acheck;
+	}
+	public String getQna_Acontent() {
+		return qna_Acontent;
+	}
+	public void setQna_Acontent(String qna_Acontent) {
+		this.qna_Acontent = qna_Acontent;
+	}
+	public String getQna_Qdate() {
+		return qna_Qdate;
+	}
+	public void setQna_Qdate(String qna_Qdate) {
+		this.qna_Qdate = qna_Qdate;
+	}
+	public String getQna_Adate() {
+		return qna_Adate;
+	}
+	public void setQna_Adate(String qna_Adate) {
+		this.qna_Adate = qna_Adate;
+	}
+	
+}
