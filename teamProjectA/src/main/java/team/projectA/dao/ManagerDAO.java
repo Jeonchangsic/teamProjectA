131fb2e2c6b1695dc83a013f1c9a97fd91a72007
798 <798@503-14>
2022-12-09 16:42:52 +0900

    jy manager 숙소등록승인

--- teamProjectA/src/main/java/team/projectA/dao/ManagerDAO.java
+++ teamProjectA/src/main/java/team/projectA/dao/ManagerDAO.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import team.projectA.vo.LodgingVO;
 import team.projectA.vo.RoomVO;
+import team.projectA.vo.UserVO;
 
 @Repository
 public class ManagerDAO {
@@ -19,8 +20,20 @@ public class ManagerDAO {
 	public List<LodgingVO> lodgingCategory() throws Exception{
 		return sqlSession.selectList("team.projectA.mapper.ManagerMapper.lodgingCategory");
 	}
-	public List<RoomVO> selectRoomList(String lidx){
+	public List<RoomVO> selectRoomList(int lidx){
 		
 		return sqlSession.selectList("team.projectA.mapper.ManagerMapper.lodgingID",lidx);
+	}
+	public List<LodgingVO> requestList() {
+		return sqlSession.selectList("team.projectA.mapper.ManagerMapper.requestList");
+	}
+	public int approval(int uidx) {
+		return sqlSession.update("team.projectA.mapper.ManagerMapper.approval", uidx);
+	}
+	public int requestDel(int lidx) {
+		return sqlSession.delete("team.projectA.mapper.ManagerMapper.requestDel", lidx);
+	}
+	public int requestN(int uidx) {
+		return sqlSession.update("team.projectA.mapper.ManagerMapper.requestN", uidx);
 	}
 }
--- teamProjectA/src/main/java/team/projectA/service/ManagerService.java
+++ teamProjectA/src/main/java/team/projectA/service/ManagerService.java
@@ -1,11 +1,17 @@
 package team.projectA.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import team.projectA.vo.LodgingVO;
 import team.projectA.vo.RoomVO;
+import team.projectA.vo.UserVO;
 
 public interface ManagerService {
 	public List<LodgingVO> lodgingCategory()throws Exception;
-	public List<RoomVO> selectRoomList(String lidx);
+	public List<RoomVO> selectRoomList(int lidx);
+	public List<LodgingVO> requestList();
+	public int approval(int uidx);
+	public int requestDel(int lidx);
+	public int requestN(int uidx);
 }
