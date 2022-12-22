035ea0e85734eba4a65ff99546535e0c6e8b8ed6
MYCOM <MYCOM@503-10>
2022-12-22 17:01:08 +0900

    333

---
+++ teamProjectA/src/main/java/team/projectA/vo/MultiUploadVO.java
@@ -0,0 +1,16 @@
+package team.projectA.vo;
+
+import java.util.List;
+
+public class MultiUploadVO {
+	
+private List<RoomVO> list;
+
+public List<RoomVO> getList() {
+	return list;
+}
+
+public void setList(List<RoomVO> list) {
+	this.list = list;
+}
+}
